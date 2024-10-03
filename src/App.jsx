89efc3acadd012851eeba6f6e71a17bcb5c8e1a4
Agustín Servin <agustinservin199103@gmail.com>
2024-10-03 19:56:01 -0300

    Agregamos ruta y estructura del login

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
+import { BrowserRouter as  Router, Routes, Route} from 'react-router-dom'
+import Login from './components/Auth/Login'
+import Home from './components/Home/Home'
+
 function App() {
   
   return (
     <>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/login' element={<Login/>}/>
+        
+      </Routes>
+    </Router>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import './index.css';
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <NextUIProvider>
-     <App />
+    <App />
     </NextUIProvider>
   </StrictMode>,
 )
